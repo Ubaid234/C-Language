167f3eb2848656f89c4980dde9bf17d9dc5ce8db
ubaid <mrubaid56@gmail.com>
2022-06-16 21:36:44 +0530

    Assigning and Declaring value

--- Instruction-and-operators/main.c
+++ Instruction-and-operators/main.c
@@ -3,13 +3,17 @@
 int main() {
     
     // int a = 4; // Type declaration Instruction
-    int a = 4, b ,c; // Type declaration instruction
-    b = c = a;
+    // int a = 4, b ,c; // Type declaration instruction
+    // b = c = a;
 
-    printf("The Value of a is %d \n" , a);
-    printf("The Value of b is %d \n" , b);
-    printf("The Value of c is %d \n" , c);
+    // printf("The Value of a is %d \n" , a);
+    // printf("The Value of b is %d \n" , b);
+    // printf("The Value of c is %d \n" , c);
 
+    // return 0;
 
-    return 0;
+    float b = a + 8.9 ;
+    float a = 1.1;
+
+    printf("The value of b is %f \n", b);
 }
