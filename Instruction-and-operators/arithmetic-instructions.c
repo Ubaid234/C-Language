ff56c7d75f53ef192b3799a910e313848b7151ae
ubaid <mrubaid56@gmail.com>
2022-06-16 21:47:06 +0530

    Arithmetic instructions

---
+++ Instruction-and-operators/arithmetic-instructions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    
+    int a = 4 ;
+    int b = 3 ;
+
+    printf("The Value of a  + b is %d \n" , a + b);
+    printf("The Value of a  - b is %d \n" , a - b);
+    printf("The Value of a x b is %d \n" , a * b);
+    printf("The Value of a / b is %d \n" , a / b);
+
+
+    return 0;
+}                       
