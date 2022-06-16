46d097ab1b1c24c2db0dc71d92de72bfb131c4f0
ubaid <mrubaid56@gmail.com>
2022-06-16 21:52:07 +0530

    Legal and illegal assignment

--- Instruction-and-operators/arithmetic-instructions.c
+++ Instruction-and-operators/arithmetic-instructions.c
@@ -4,12 +4,14 @@ int main() {
     
     int a = 4 ;
     int b = 3 ;
+    int z;
+    z = a * b; // legal
+    // b * a = z; // illegal 
 
     printf("The Value of a  + b is %d \n" , a + b);
     printf("The Value of a  - b is %d \n" , a - b);
-    printf("The Value of a x b is %d \n" , a * b);
+    printf("The Value of a x b is %d \n" , z);
     printf("The Value of a / b is %d \n" , a / b);
-
 
     return 0;
 }                       
