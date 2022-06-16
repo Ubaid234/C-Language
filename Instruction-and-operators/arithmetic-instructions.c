c43f77a08e361299f782cb4b2c5ff9c450056307
ubaid <mrubaid56@gmail.com>
2022-06-16 22:02:27 +0530

    correct assignment and placement of operators

--- Instruction-and-operators/arithmetic-instructions.c
+++ Instruction-and-operators/arithmetic-instructions.c
@@ -13,5 +13,14 @@ int main() {
     printf("The Value of a x b is %d \n" , z);
     printf("The Value of a / b is %d \n" , a / b);
 
+    printf("5 when divided by 2 leaves a reminder of %d \n " , 5%2);
+     
+    // No operator is assumed to be present  
+    // printf("The value of 4 * 5 is %d \n " ,(4)(5) ); // shows error
+
+    // Operator is to be placed to perform the function
+
+    printf(" The value of 4 and 5 %d \n " , 4 * 5);
+
     return 0;
 }                       
