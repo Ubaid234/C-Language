2b722d52b62bdc67d70160dd4d5e996d4c3f65ee
ubaid <mrubaid56@gmail.com>
2022-06-16 22:56:44 +0530

    Operator Precedence

---
+++ Instruction-and-operators/Operator-Precendence.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (){
+
+    // Operator Precedence 
+
+    int a = 4;
+    int b = 3;
+
+    printf("The value of 3*a - 8*b is %d \n " , 3*a - 8*b);
+
+    return 0;
+}
