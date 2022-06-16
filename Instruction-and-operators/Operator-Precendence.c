d301c11698c1f825088e8619e557467f6a600ecd
ubaid <mrubaid56@gmail.com>
2022-06-16 22:58:39 +0530

    Operator Associativity

--- Instruction-and-operators/Operator-Precendence.c
+++ Instruction-and-operators/Operator-Precendence.c
@@ -9,5 +9,13 @@ int main (){
 
     printf("The value of 3*a - 8*b is %d \n " , 3*a - 8*b);
 
+    // Operator Associativity in case of equal operator precendence
+
+    printf("The value of 8*b / 3*a is %d \n ", 8*b / 3*a);
+    // 8*3/3*a = 24 
+    // 24/3*4
+    // 8 * 4
+    // 32
+
     return 0;
 }
