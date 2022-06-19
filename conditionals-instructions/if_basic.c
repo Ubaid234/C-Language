9976df8f8c5d73751238ffcdf3deadeb58f4ac16
ubaid <mrubaid56@gmail.com>
2022-06-19 14:55:17 +0530

    conditionals instructions

---
+++ conditionals-instructions/if_basic.c
@@ -0,0 +1,21 @@
+
+// C program to check whether the Program is ODD or Even .
+
+#include<stdio.h>
+
+int main(){
+
+    int a , b;
+    printf("Enter a number \n");
+    scanf("%d", &a);
+
+
+    if (a%2==0) {
+        printf("%d is even \n", a);
+    }
+    else {
+        printf("%d is odd \n", a);
+    }
+
+    return 0;
+}
