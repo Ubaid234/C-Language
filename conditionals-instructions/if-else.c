a503551e1cd498b5c19b467114ab04a094ba6a6a
ubaid <mrubaid56@gmail.com>
2022-06-19 15:10:24 +0530

    Comparison Operators

--- conditionals-instructions/if-else.c
+++ conditionals-instructions/if-else.c
@@ -6,10 +6,14 @@ int main() {
     printf("Enter the age \n ");
     scanf("%d" , &age);
     if ( age > 90) {
-        printf("You are above 90 thus you can't drive");
+        printf("You are above 90 thus you can't drive \n");
     }
     else{
-        printf("You can drive");
+        printf("You can drive \n");
+    }
+
+    if ( age == 50 ) {
+        printf("Half Century \n");
     }
 
     return 0;
