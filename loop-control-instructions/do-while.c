c1c776e1e1c6d94567960a8a635d596edbeceb60
ubaid <mrubaid56@gmail.com>
2022-07-03 16:32:20 +0530

    Do  While Loops

---
+++ loop-control-instructions/do-while.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    
+    int a ;
+    a = 0;
+
+    do {
+        printf(" THE VALUE OF A  IS %d\n", a);
+        a++;
+    } 
+    while ( a < 10);
+    
+    return 0;
+}
